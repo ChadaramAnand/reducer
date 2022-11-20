2432cc965e04322d49686911831e3e0dcc2dae64
ChadaramAnand <113660841+ChadaramAnand@users.noreply.github.com>
2022-11-20 08:51:14 +0530

    updated counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -8,7 +8,7 @@ const Count = () => {
     const [value , dispatch] = useReducer(Reducer,initialState)
   return (
     <div>
-        <h1>counter</h1>
+        <h1>counter using reducer </h1>
         <button onClick={()=>{dispatch({type:'Increment',payload:value.num+1})}}>+</button>
         <div>{value.num}</div>
         <button onClick={()=>{dispatch({type:'Decrement'})}}>-</button>
